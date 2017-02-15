4c3a6dc29b3f26bc0c8088f185a026d0b52a838c
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-15 12:20:20 +0100

    add SpaceAfter=No using ud.SetSpaceAfter

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# TODO add ud.SetSpaceAfter
-SCENARIO="ud.Convert1to2 ud.he.FixNeg"
+udapy -h >/dev/null || { echo "udapy is not installed, see https://github.com/udapi/udapi-python"; exit 1; }
+
+SCENARIO="ud.Convert1to2 ud.he.FixNeg ud.SetSpaceAfter"
 for a in train dev test; do
     cat he-ud-$a.conllu | udapy -s $SCENARIO util.Eval bundle="bundle.bundle_id = '$a-'+bundle.bundle_id" > ../he-ud-$a.conllu
 done
